88a2a674b6c010c23849b201f5de7279e7f29ddd
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2021-12-13 13:55:09 +0100

    modificando pagina de usuarios

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -7,19 +7,87 @@ import TimeAgo from 'react-timeago';
 import env from 'react-dotenv';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useParams } from 'react-router';
+import { link } from 'fs';
 
 const Users: FC = () => {
 
-    const {id} = useParams();
-    
     const [currentUser, setCurrentUser] = useState<User | null>(null);
-
+    const {id} = useParams();
+    const [creator, setCreator] = useState<User | null>(null);
+    const { state } = useContext(AuthContext);
     const isSignedIn = useSelector((state:any)=> state.auth.isSignedIn);
+
+    useEffect(()=>{
+        setCurrentUser(state.user)
+      },[state.user])
+
+    useEffect(()=>{user_service.updateUser(currentUser)},[currentUser])
+
+    useEffect(()=>{
+        setCurrentUser(state.user);
+        if(id) user_service.getUser(id).then(user=>setCreator(user))
+        },[])
+
+    
+
+
+    const sameUser = () => {
+        if(currentUser)
+            if(creator)
+                return (currentUser.id == creator.id)
+    }
 
 
     return(
         <>
-            <h1>Perfil de Usuario</h1>
+            <div className='userType'>
+                <table className='table'>
+                    <tr>
+                        <td className='c-gray' valign="top"> user:</td> 
+                        <td className='c-gray'> {creator?.full_name}</td>
+                    </tr>
+                    <tr>
+                        <td className='c-gray' valign="top"> created:</td> 
+                        <td className='c-gray'> 
+                            {
+                                creator?
+                                <TimeAgo date={creator.created_at}></TimeAgo>
+                                :null
+                            }
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='c-gray' valign="top"> karma:</td> 
+                        <td className='c-gray'> {creator?.karma}</td>
+                    </tr>
+                    <tr>
+                        <td className='c-gray' valign="top"> about:</td> 
+                        <td className='c-gray'> {creator?.about}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className='link'> submissions
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className='link'> comments
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td className='link'> favorites
+                        </td>
+                    </tr>
+                </table>
+                <h1>Tipo de perfil: </h1>
+                {
+                    sameUser()?
+                    {}
+                    :
+                    {}
+                }
+            </div>
         </>
     )
 }
